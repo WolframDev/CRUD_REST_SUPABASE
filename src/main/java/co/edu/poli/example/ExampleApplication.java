c2638444b215770a1afd6db3e29cf7a9ac0ca571
Wsoto <wsoto@gmail.com>
2026-03-08 12:40:21 -0500

    upd:configure .env

--- src/main/java/co/edu/poli/example/ExampleApplication.java
+++ src/main/java/co/edu/poli/example/ExampleApplication.java
@@ -8,10 +8,17 @@ import io.github.cdimascio.dotenv.Dotenv;
 @SpringBootApplication
 public class ExampleApplication {
 
-	public static void main(String[] args) {	
-		Dotenv dotenv = Dotenv.configure().load();
-        System.setProperty("DB_USER_SUPABASE", dotenv.get("DB_USER_SUPABASE"));
-        System.setProperty("DB_PWD_SUPABASE", dotenv.get("DB_PWD_SUPABASE"));	
+	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure()
+        .ignoreIfMissing()
+        .load();
+		String dbUser = dotenv.get("DB_USER_SUPABASE", System.getenv("DB_USER_SUPABASE"));
+		String dbPwd = dotenv.get("DB_PWD_SUPABASE", System.getenv("DB_PWD_SUPABASE"));
+
+		System.setProperty("DB_USER_SUPABASE", dbUser);
+		System.setProperty("DB_PWD_SUPABASE", dbPwd);
+        //System.setProperty("DB_USER_SUPABASE", System.getenv("DB_USER_SUPABASE"));
+        //System.setProperty("DB_PWD_SUPABASE", System.getenv("DB_PWD_SUPABASE"));	
 		SpringApplication.run(ExampleApplication.class, args);
 	}
 
