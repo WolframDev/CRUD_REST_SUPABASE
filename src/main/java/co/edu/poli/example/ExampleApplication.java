4d8a525ea11cfdf884c897e826e4be5d5ac131e7
WSoto <ing.wsoto@gmail.com>
2026-05-19 16:27:23 -0500

    Comment out dotenv setup in ExampleApplication
    Comment out dotenv configuration for database credentials.

--- src/main/java/co/edu/poli/example/ExampleApplication.java
+++ src/main/java/co/edu/poli/example/ExampleApplication.java
@@ -9,14 +9,14 @@ import io.github.cdimascio.dotenv.Dotenv;
 public class ExampleApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.configure()
+		/*Dotenv dotenv = Dotenv.configure()
         .ignoreIfMissing()
         .load();
 		String dbUser = dotenv.get("DB_USER_SUPABASE", System.getenv("DB_USER_SUPABASE"));
 		String dbPwd = dotenv.get("DB_PWD_SUPABASE", System.getenv("DB_PWD_SUPABASE"));
 
 		System.setProperty("DB_USER_SUPABASE", dbUser);
-		System.setProperty("DB_PWD_SUPABASE", dbPwd);
+		System.setProperty("DB_PWD_SUPABASE", dbPwd);*/
 		SpringApplication.run(ExampleApplication.class, args);
 	}
 
